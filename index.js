5a1d156ba4a0c0268c9796d2dab6881b8bf1da71
Jonathan Thorne <jon@jonthorne.com>
2025-09-26 23:13:58 -0400

    Switched to NPM

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const token = process.env.DJMV_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+const phrases = ['GOD ALWAYS!', 'INDIE TRIBE SAVES LIVES','TALK TO ME NICE']
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (Math.floor(Math.random() * 116) + 1 == 16 || message.mentions.has(client.user.id)){
+        message.reply(phrases[Math.floor(Math.random() * 3)]);
+    }
+});
+client.login(token);
